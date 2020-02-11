4841dd7f5f33e9b36faf4511090988fe7d2fd925
Jakob Udsholt <jakob@udsholt.dk>
2020-02-11 18:10:40 +0100

    pull unstable directly in overlay

--- overlay/unstable/default.nix
+++ overlay/unstable/default.nix
@@ -1,4 +1,8 @@
-{ unstable }: self: super:
+# This overlay allows importing specific packages from unstable
+self: super:
+let
+  unstable = import <nixos-unstable> {};
+in
 {
   go_1_13_unstable = unstable.go_1_13;
   terraform_0_12_unstable = unstable.terraform_0_12;
