5f108ba8c8e300bf8abdc0895aab3efe51851772
Jakob Udsholt <jakob@udsholt.dk>
2020-03-01 01:01:44 +0100

    pickups

--- overlay/custom/istio/default.nix
+++ overlay/custom/istio/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "istio-${version}";
-  version = "1.4.4";
+  version = "1.4.5";
 
   src = fetchurl {
     url = "https://github.com/istio/istio/releases/download/${version}/istio-${version}-linux.tar.gz";
-    sha256 = "09wwaz3n70bagcw79dmqcp9020rylvk38k6pzcj2qpkciqh6alqh";
+    sha256 = "1kw2g8v0pw0iw1rs2zv56dy4mli72n9ih1ywcpwlx6zy5k136zcc";
   };
 
   phases = [ "unpackPhase" "installPhase" ];
